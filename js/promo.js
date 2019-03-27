16ecdb8b31f95d2f1c73e748e08d80c74e9517e3
Térence FERUT <46047190+TerenceSimplon@users.noreply.github.com>
2019-03-27 11:27:47 +0100

    Add files via upload

--- js/promo.js
+++ js/promo.js
@@ -17,15 +17,20 @@
 			var html = "<div class='row header'><div class='cell'>Table n°</div><div class='cell'>Apprenant</div><div class='cell'>Apprenant</div></div>" ;
 			for (i = 0 ; i < myArray.length / 2 ; i++)
 			{
+				var appr1 = myArray[2*i] ;
+				var appr2 = myArray[2*i + 1] ;
+				if (appr1 == undefined) appr1 = "" ;
+				if (appr2 == undefined) appr2 = "" ;
+				
 				html += "<div class='row'>"
 							+"<div class='cell' data-title='Table'>"+
 								(i+1)
 							+"</div>"
 							+"<div class='cell' data-title='Apprenant'>"+
-								myArray[2*i]
+								appr1
 							+"</div>"
 							+"<div class='cell' data-title='Apprenant'>"+
-								myArray[2*i + 1]
+								appr2
 							+"</div>"
 						+"</div>" ;
 			}
@@ -54,29 +59,30 @@
 
 		var myArray = [
 		'Alban',
+		'Tanguy',
 		'Théo',
 		'Tunga',
 		'Karima',
 		'Florent',
-		'Tanguy',
 		'Marine',
-		'Nada',
 		'Georges',
 		'Pierre',
 		'Thomas',
+		'Aleksandra',
 		"Quentin",
 		'Loïc B',
+		'Nada',
 		'Loïc C',
 		'Maria',
 		'Victoria',
-		'Anabela',
 		'Aladin',
 		'Julia',
+		'Anabela',
 		'Dylan',
 		'Anissa',
 		'Alexandre',
-		'Aleksandra',
-		'Rael'
+		'Rael',
+		'Michel',
 		];
 		
 
@@ -85,7 +91,7 @@
 			"Calcul des risques de clash ...",
 			"Détection de bugs dus au rayonnement cosmique ...",
 			"Backtracking dans l'arbre des combinatoires ...",
-			"Vérification de l'alignement des étoiles ...",
+			"Vérification de l'alignement des planètes ...",
 			"Altération des capacités sensitives ...",
 			"Approximation des compatibilités amoureuses ...",
 			"Prise en compte de la météo ...",
@@ -95,4 +101,21 @@
 			"Dernières vérifications ...",
 			"Préparation de l'affichage des résultats"
 			];
+		myArray = shuffle(myArray);
+		myArray = shuffle(myArray);
+		myArray = shuffle(myArray);
+		myArray = shuffle(myArray);
+		myArray = shuffle(myArray);
+		myArray = shuffle(myArray);
+		myArray = shuffle(myArray);
+		myArray = shuffle(myArray);
+		myArray = shuffle(myArray);
+		myArray = shuffle(myArray);
+		myArray = shuffle(myArray);
+		myArray = shuffle(myArray);
+		myArray = shuffle(myArray);
+		myArray = shuffle(myArray);
+		myArray = shuffle(myArray);
+		myArray = shuffle(myArray);
+		myArray = shuffle(myArray);
 		myArray = shuffle(myArray);
